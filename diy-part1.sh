df23ae7414dd810ef56620101f52efc46f79bc2a
lyonji <72914496+lyonji@users.noreply.github.com>
2022-01-21 15:11:45 +0800

    Create diy-part1.sh

---
+++ diy-part1.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+#
+# Copyright (c) 2019-2020 P3TERX <https://p3terx.com>
+#
+# This is free software, licensed under the MIT License.
+# See /LICENSE for more information.
+#
+# https://github.com/P3TERX/Actions-OpenWrt
+# File name: diy-part1.sh
+# Description: OpenWrt DIY script part 1 (Before Update feeds)
+#
+
+# Uncomment a feed source
+#sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
+
+# Add a feed source
+echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
+# echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
+# echo 'src-git kenzo https://github.com/kenzok8/openwrt-packages' >>feeds.conf.default
+# echo 'src-git small https://github.com/kenzok8/small' >>feeds.conf.default
+cd openwrt
+cd package
+git clone https://github.com/destan19/openfros.git
+git clone https://github.com/Hill-98/luci-app-k3screenctrl.git
+git clone https://github.com/Hill-98/openwrt-k3screenctrl.git
+cd ..
+source /etc/environment
+./scripts/feeds update -a && ./scripts/feeds install -a
